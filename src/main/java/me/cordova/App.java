02077295222fa726a96397d5ddf020b3d17f114b
charles.cordova <charles@cordova.me>
2019-05-12 00:34:05 -0500

    add product list

--- src/main/java/me/cordova/App.java
+++ src/main/java/me/cordova/App.java
@@ -1,13 +1,17 @@
 package me.cordova;
 
 
+import me.cordova.model.Brand;
 import me.cordova.model.Item;
+import me.cordova.model.Product;
+import me.cordova.model.Product.ProductBuilder;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,25 +26,9 @@ public class App {
         //document = Jsoup.connect("https://www.metro.pe/abarrotes").get();
         document = Jsoup.connect("https://www.metro.pe/bebidas").get();
 
-        //elements = getElementByClass("main");
-
         List<Item> items;
 
         elements = document.select(".product-item__info");
-
-/*      System.out.println("itemNodes ===================================================");
-        System.out.println(itemNodes.outerHtml());
-
-        itemNodes.forEach(
-                element -> {
-                    System.out.println("itemNodes element ===================================================");
-                    //System.out.println(element.attributes());
-                    //System.out.println(element.baseUri());
-                    //System.out.println(element.child(0));
-                    //System.out.println(element.children());
-                    System.out.println(element.getElementsByAttribute("href"));
-                }
-        );*/
 
         items = elements.stream()
                 .map(element ->
@@ -50,15 +38,14 @@ public class App {
                 .collect(Collectors.toList());
 
         System.out.println("===================================================");
-        System.out.println("========== #1 Found " + items.size() + " items =========================");
+        System.out.println("========== #1 Found " + items.size() + " items ====");
         System.out.println("===================================================");
         System.out.println(items);
         System.out.println("===================================================");
-
-
-        System.out.println("===================================================");
         System.out.println("========== #2 Found Details for each items ========");
         System.out.println("===================================================");
+
+        List<Product> products = new ArrayList<>();
 
         Integer i = 0;
 
@@ -69,41 +56,46 @@ public class App {
             document = Jsoup.connect(url).get();
 
             System.out.println("===================================================");
-            System.out.println( "# " + i++ + " " + document.title());
+            System.out.println( "========== P " + i++ + " " + document.title());
             System.out.println("===================================================");
 
-            //final Elements productContent = document.getAllElements();
-
-            //Elements productInfo = document.getElementsByClass("product-info");
-
-/*            System.out.println("product-info ===================================================");
-            elements = getElementByClass("product-info");
-            System.out.println(elements);*/
-
-            //System.out.println("info-wrapper ===================================================");
-            elements = getElementByClass("info-wrapper");
-            //System.out.println(elements.outerHtml());
-
-            System.out.println("Product name ====================================================");
-            System.out.println(elements.select(".name").first().text());
-            System.out.println("Product brand ===================================================");
-            System.out.println(elements.select(".brand").first().text());
-            System.out.println("Product description =============================================");
-            System.out.println(elements.select(".productDescription").first().text());
-            System.out.println("Product price ===================================================");
-            System.out.println(elements.select(".skuBestPrice").first().text());
-            System.out.println("Product image ===================================================");
-            //elements = getElementByClass("image");
-            //System.out.println(elements.select("#image").first().outerHtml());
-            System.out.println(getElementByClass("image").select("#image").first().getElementsByAttribute("href").attr("href"));
+            products.add(createProduct());
 
         }
-
+        System.out.println(products.toString());
+        System.out.println("===================================================");
 
     }
 
-    private static Elements getElementByClass(String className) {
+    private static Product createProduct() {
 
-        return document.select(".".concat(className));
+        elements = document.select(".info-wrapper");
+
+        String name = elements.select(".name").first().text();
+        String description = elements.select(".productDescription").first().text();
+        Double price = getProductPrice(elements.select(".skuBestPrice").first().text());
+        Brand brand = getProductBrand(elements.select(".brand").first().text());
+        String image = document.select(".image").select("#image").first().getElementsByAttribute("href").attr("href");
+        Integer stock = 100;
+
+        Product product = Product.getBuilder()
+                .withName(name)
+                .withDescription(description)
+                .withPrice(price)
+                .withBrand(brand)
+                .withImage(image)
+                .withStock(stock)
+                .build();
+
+        return product;
     }
+
+    private static Brand getProductBrand(String brand) {
+        return new Brand(brand,"");
+    }
+
+    private static Double getProductPrice(String price) {
+        return 15.6;
+    }
+
 }
--- src/main/java/me/cordova/model/Product.java
+++ src/main/java/me/cordova/model/Product.java
@@ -2,17 +2,12 @@ package me.cordova.model;
 
 public class Product {
 
-    String name;
-    Brand brand;
-    String image;
-    Integer stock;
-
-    public Product(String name, Brand brand, String image, Integer stock) {
-        this.name = name;
-        this.brand = brand;
-        this.image = image;
-        this.stock = stock;
-    }
+    private String name;
+    private String description;
+    private Double price;
+    private Brand brand;
+    private String image;
+    private Integer stock;
 
     public String getName() {
         return name;
@@ -20,6 +15,22 @@ public class Product {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public Brand getBrand() {
@@ -50,9 +61,73 @@ public class Product {
     public String toString() {
         return "Product{" +
                 "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
                 ", brand=" + brand.getName() +
                 ", image='" + image + '\'' +
                 ", stock=" + stock +
                 '}';
+    }
+
+    public static ProductBuilder getBuilder() {
+        return new ProductBuilder();
+    }
+
+    public static class ProductBuilder {
+
+        private String name;
+        private String description;
+        private Double price;
+        private Brand brand;
+        private String image;
+        private Integer stock;
+
+        private Product product;
+
+        public ProductBuilder withName(String name){
+            this.name = name;
+            return this;
+        }
+
+        public ProductBuilder withDescription(String description){
+            this.description = description;
+            return this;
+        }
+
+        public ProductBuilder withPrice(Double price){
+            this.price = price;
+            return this;
+        }
+
+        public ProductBuilder withBrand(Brand brand){
+            this.brand = brand;
+            return this;
+        }
+
+        public ProductBuilder withImage(String image){
+            this.image = image;
+            return this;
+        }
+
+        public ProductBuilder withStock(Integer stock){
+            this.stock = stock;
+            return this;
+        }
+
+        public Product build() {
+            this.product = new Product();
+            product.setName(this.name);
+            product.setDescription(this.description);
+            product.setPrice(this.price);
+            product.setBrand(this.brand);
+            product.setImage(this.image);
+            product.setStock(this.stock);
+            return this.product;
+        }
+
+        public Product getProduct() {
+            return this.product;
+        }
+
     }
 }
